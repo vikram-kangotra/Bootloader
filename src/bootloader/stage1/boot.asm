c30f3f5870f14e84e0d7c4ee7fc7949ca11a08fb
Vikram-kangotra <vikramkangotra8055@gmail.com>
2023-02-02 15:53:30 +0530

    Added BIOS Parameter Block for FAT12 filesystem

--- src/bootloader/stage1/boot.asm
+++ src/bootloader/stage1/boot.asm
@@ -3,35 +3,41 @@ bits 16
 
 %define ENDL 0x0a, 0x0d
 
-mov si, hello
-call puts
+; FAT12 header
+
+jmp short start
+nop
+
+bpb_oem:                        db "MSWIN4.1"               ; Just because Microsoft recommends it
+bpb_bytes_per_sector:           dw 512                      ; 512 bytes per sector
+bpb_sectors_per_cluster:        db 1                        ; 1 sector per cluster
+bpb_reserved_sectors:           dw 1                        ; 1 reserved sector
+bpb_number_of_fats:             db 2                        ; 2 FATs
+bpb_root_dir_entries:           dw 0xE0                     ; 224 root directory entries
+bpb_total_sectors:              dw 0x0b40                   ; 2880 sectors
+bpb_media_descriptor:           db 0xf0                     ; 3.5" 1.44MB floppy
+bpb_sectors_per_fat:            dw 0x09                     ; 9 sectors per FAT
+bpb_sectors_per_track:          dw 0x12                     ; 18 sectors per track
+bpb_number_of_heads:            dw 0x02                     ; 2 heads
+bpb_number_of_hidden_sectors:   dd 0                        ; 0 hidden sectors
+bpb_large_sector_count:         dd 0                        ; 0 large sector count
+
+; extended BIOS parameter block
+
+ebpb_physical_drive_number:     db 0x00                     ; physical drive number
+ebpb_current_head:              db 0x00                     ; current head
+ebpb_extended_boot_signature:   db 0x29                     ; extended boot signature
+ebpb_volume_id:                 dd 0x12345678               ; volume ID
+ebpb_volume_label:              db "MIOSIS     ", 0         ; volume label
+ebpb_file_system_type:          db "FAT12   ", 0            ; file system type
+
+start:
+    mov si, hello
+    call puts
 
 jmp halt
 
-puts:
-    push si
-    push ax
-    push bx
-
-.loop:
-    lodsb
-    or al, al
-    jz .done
-
-    mov ah, 0x0e
-    mov bh, 0x00
-    int 0x10
-    jmp .loop
-
-.done:
-    pop bx
-    pop ax
-    pop si
-    ret
-
-halt:
-    cli
-    hlt
+%include "utility.asm"
 
 hello: db 'Hello, world!', ENDL, 0x00
 
