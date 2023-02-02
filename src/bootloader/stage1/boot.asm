a6c9aca3d95030b99cc04d47304c6c00cbe9b877
Vikram-kangotra <vikramkangotra8055@gmail.com>
2023-02-02 17:16:48 +0530

    Tested the disk read function

--- src/bootloader/stage1/boot.asm
+++ src/bootloader/stage1/boot.asm
@@ -19,7 +19,7 @@ bpb_media_descriptor:           db 0xf0                     ; 3.5" 1.44MB floppy
 bpb_sectors_per_fat:            dw 0x09                     ; 9 sectors per FAT
 bpb_sectors_per_track:          dw 0x12                     ; 18 sectors per track
 bpb_number_of_heads:            dw 0x02                     ; 2 heads
-bpb_number_of_hidden_sectors:   dd 0                        ; 0 hidden sectors
+bpb_hidden_sectors:             dd 0                        ; 0 hidden sectors
 bpb_large_sector_count:         dd 0                        ; 0 large sector count
 
 ; extended BIOS parameter block
@@ -28,18 +28,35 @@ ebpb_physical_drive_number:     db 0x00                     ; physical drive num
 ebpb_current_head:              db 0x00                     ; current head
 ebpb_extended_boot_signature:   db 0x29                     ; extended boot signature
 ebpb_volume_id:                 dd 0x12345678               ; volume ID
-ebpb_volume_label:              db "MIOSIS     ", 0         ; volume label
-ebpb_file_system_type:          db "FAT12   ", 0            ; file system type
+ebpb_volume_label:              db "MIOSIS     "            ; volume label
+ebpb_file_system_type:          db "FAT12   "               ; file system type
 
 start:
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+
+    mov ss, ax
+    mov sp, 0x7c00
+
+    mov [ebpb_physical_drive_number], dl
+
+    mov ax, 1
+    mov cl, 1
+    mov bx, buffer
+    call disk_read
+
     mov si, hello
     call puts
 
 jmp halt
 
 %include "utility.asm"
+%include "disk.asm"
 
 hello: db 'Hello, world!', ENDL, 0x00
 
 times 510-($-$$) db 0
 dw 0xaa55
+
+buffer:
