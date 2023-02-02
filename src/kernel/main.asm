746aa99684490b35fa092b4f93a77678697933c0
Vikram-kangotra <vikramkangotra8055@gmail.com>
2023-02-02 22:28:08 +0530

    kernel load test

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,6 +1,28 @@
-; code for halting
+mov si, hello
+call puts
 
-halt:
-    jmp halt
+cli
+hlt
 
-db "Testing for FAT12", 0x0D, 0x0A, 0x00
+puts:
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb
+    or al, al
+    jz .done
+
+    mov ah, 0x0e
+    mov bh, 0x00
+    int 0x10
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si
+    ret
+
+hello: db "Hello World from Kernel", 0x0a, 0x0d, 0
